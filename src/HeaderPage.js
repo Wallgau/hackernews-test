3c139618f6b49279d68773a256cb4f8b0df2ec41
Wallgau <maslah.olfa@gmail.com>
2019-12-18 11:23:53 -0500

    style pages mobile first  WIP

--- src/HeaderPage.js
+++ src/HeaderPage.js
@@ -1,55 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import MenuRoundedIcon from "@material-ui/icons/MenuRounded";
 import CloseIcon from "@material-ui/icons/Close";
-import { Header, Article } from "./styled.js";
+import {Header, Article} from "./styled.js";
 
 function HeaderPage() {
-  const [open, setOpen] = useState(false);
-  console.log(open);
-  useEffect(() => {}, []);
-  return (
-    <>
-      <Header>
-        <Article white>
-          <div className="w">W</div>
-        </Article>
-        {open ? (
-          <div>
-            <nav>
-              <CloseIcon onClick={() => setOpen(!open)} />
-              <ul>
-                <li>
-                  <a>The details</a>
-                </li>
-                <li>
-                  <a>magazine</a>
-                </li>
-                <li>
-                  <a>who we are</a>
-                </li>
-                <li>
-                  <a>sign in</a>
-                </li>
-              </ul>
-              <section>
-                <a>start investing</a>
-                <div>
-                  <a></a>
-                  <a></a>
-                </div>
-              </section>
-            </nav>
-          </div>
-        ) : (
-          <Article white>
-            <MenuRoundedIcon
-              className="hamburger"
-              onClick={() => setOpen(!open)}
-            />
-          </Article>
-        )}
-      </Header>
-    </>
-  );
+    const [open, setOpen] = useState(false);
+    console.log(open);
+    useEffect(() => {}, []);
+    return (<>
+        <Header> {
+            open ? (<div>
+                <nav>
+                    <CloseIcon onClick={
+                        () => setOpen(!open)
+                    }/>
+                    <ul>
+                        <li>
+                            <a>The details</a>
+                        </li>
+                        <li>
+                            <a>magazine</a>
+                        </li>
+                        <li>
+                            <a>who we are</a>
+                        </li>
+                        <li>
+                            <a>sign in</a>
+                        </li>
+                    </ul>
+                    <section>
+                        <a>start investing</a>
+                        <div>
+                            <a></a>
+                            <a></a>
+                        </div>
+                    </section>
+                </nav>
+            </div>) : (<div>
+                <Article>
+
+                    <div className="w">W</div>
+                    <MenuRoundedIcon className="hamburger"
+                        onClick={
+                            () => setOpen(!open)
+                        }/>
+                </Article>
+            </div>)
+        } </Header>
+    </>);;
 }
 export default HeaderPage;
--- src/styled.js
+++ src/styled.js
@@ -1,48 +1,61 @@
 import React from "react";
-import styled, {withTheme} from "styled-components";
+import styled, {keyframes} from "styled-components";
+
 
 export const Header = styled.div `
+
 @media (max-width: 768px) {
-	
-  display: flex;
-	background:white;
-	z-index:15;
-	position:fixed
-	
-	top:0;
-	justify-content:space-between;
-	margin: 0;
-	padding: 1.3rem 0
-	margin-left: auto;
-    margin-right: auto;
-    max-width: 1490px;
-    padding-left: 25px;
-    padding-right: 25px;
+	& > div{
+		background:white;
+		width:97%;
+		max-width: 98%;
+		z-index:15;
+		position:fixed;
+		padding:1.3rem;
+		margin: 0 auto;
+		top:0;
+left:-0.6rem;
+	}
+	&>div>div{
+		display:flex;
+		justify-content:space-between;
+	}
+	nav{
+		background:red;
+		position:absolute;
+		left:100%;
+		height:100%;
+		z-index:200;
+		top:0;
+		position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
     width: 100%;
-		    
+	}
 	
   .w {
     border: 1px solid black;
 		background:black
     border-radius: 50%;
 		padding:0.3rem
-		height:1.5rem;
-		width:1.5rem;
+		height:2rem;
+		width:2rem;
 		font-size: 1.6rem;
+		text-align:center;
 		font-weight:600;
-    color: white;
-		margin:0;
-		
-		
+    color: white;	
   }
 	.hamburger{
 		border: 1px solid #fab131;
 		background-color: #fab131;
 		border-radius: 50%;
-		 color: white;
-		 padding:0.3rem;
-		 margin-left: 51%;
-		 
+		color: white;
+		position:relative;
+		padding:0.3rem;
+		height:2rem;
+		width:2rem;
+		transition: all 10s ease; 
 	}
 	}
 `;
@@ -53,6 +66,7 @@ export const Body = styled.div `
 	margin:0
   padding: 0;
   background: #f9f8f6;
+	border: 2px solid black;
 `;
 
 export const Img = styled.img `
@@ -61,14 +75,16 @@ export const Img = styled.img `
 `;
 
 export const Article = styled.div `
-  width: 90%;
+ width:90%;
   margin: ${
     props => props.eachArticle ? "2rem auto" : "0 auto"
 };
-	
+		border: 1px solid blue;
+		
 	& > div > div{
 		border: 1px solid red;
 		margin-bottom: 2rem
+		background:white;
 	}
 
 `;
